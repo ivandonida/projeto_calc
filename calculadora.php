26e337397dd072f5f326155212e4fa862fdbe0ed
Ivanzão <aluno@localhost.localdomain>
2015-09-17 20:07:20 -0300

    Adicionando a operação soma

---
+++ calculadora.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang = "pt-br">
+<head>
+   <title>Calculadora</title>
+   <meta charset = "UTF-8">
+</head>
+<body>
+   <form action="" method="post" >
+      Primeiro Numero: <input name="num1" type="text"><br>
+      Segundo numero: <input name="num2" type="text"><br>
+      <input type="submit" name="operacao" value="+">   
+         
+   </form> 
+<
+</body>
+</html>
